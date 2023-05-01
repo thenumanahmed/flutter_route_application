f421b3076d52d3deab18c621bf97ef33ea8e7123
numan ahmed <numanahmedmail@gmail.com>
2023-05-01 17:59:42 +0500

    collections constant added

--- lib/dbHelper/db_constants.dart
+++ lib/dbHelper/db_constants.dart
@@ -6,13 +6,14 @@
 //     "mongodb+srv://salman:1234@uetroute.4hfzd.mongodb.net/routeapp?retryWrites=true&w=majority";
 
 const MONGO_CONN_URL = "mongodb://localhost:27017/routeapp";
-const TRACKS_COLLECTION = "tracks";
-const DRIVERS_COLLECTION = "drivers";
-const MEMBERS_COLLECTION = "members";
-const ADMINS_COLLECTION = "admins";
 const BUSES_COLLECTION = "buses";
+const ADMINS_COLLECTION = "admins";
+const DRIVERS_COLLECTION = "drivers";
+const TRACKS_COLLECTION = "tracks";
+const MEMBERS_COLLECTION = "members";
 const ROUTES_COLLECTION = "routes";
 const TRACKING_COLLECTION = "tracking";
+const STOPS_COLLECTION = "stops";
 
 const user = "salman";
 // q^7]<C(2hOJmbE|b
